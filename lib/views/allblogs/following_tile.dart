b324b0ecdd75cfab07c3083db4beb9c66f12bf25
Siddhartha Malladi <msiddhu45@gmail.com>
2021-05-16 02:40:40 +0530

    added followers_list.dart should resolve dispose error in next commit.

--- lib/views/allblogs/following_tile.dart
+++ lib/views/allblogs/following_tile.dart
@@ -1,3 +1,4 @@
+import 'package:bloggie/views/profile/user_profile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,43 +8,40 @@ class FollowingCard extends StatelessWidget {
   FollowingCard(this.mpdata);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-        color: Colors.lime,
-        elevation: 5,
-        child:Row(
-
-          children:[
-            Container(
-              padding: EdgeInsets.all(10),
-              child: ClipOval(child: Image.network(mpdata['photoURL'])
+    return InkWell(
+      onTap: (){
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => UserProfile(mpdata['uid'])));
+      },
+      child: Container(
+        padding: EdgeInsets.all(8),
+        child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+          color: Colors.lime,
+          elevation: 5,
+          child:Row(
+            children:[
+              Container(
+                padding: EdgeInsets.all(10),
+                child: ClipOval(child: Image.network(mpdata['photoURL'])
+                ),
               ),
-            ),
-            SizedBox(width:25),
-            Column(
-              children: [
-                Text(mpdata['displayName'],style:TextStyle(fontWeight: FontWeight.bold,fontSize:18 ),),
-                SizedBox(height: 10,),
-                Text('Likes Count : '+mpdata['likescount'].toString()),
-                SizedBox(height: 10,),
-                Text('Number of Blogs : '+mpdata['blogscount'].toString()),
-              ],
-            )
-          ]
-        ),
+              SizedBox(width:25),
+              Column(
+                children: [
+                  Text(mpdata['displayName'],style:TextStyle(fontWeight: FontWeight.bold,fontSize:18 ),),
+                  SizedBox(height: 10,),
+                  Text('Likes Count : '+mpdata['likescount'].toString()),
+                  SizedBox(height: 10,),
+                  Text('Number of Blogs : '+mpdata['blogscount'].toString()),
+                ],
+              )
+            ]
+          ),
 
+        ),
       ),
     );
 
-    return Column(children: [
-
-      Text(mpdata['uid']),
-      Text(mpdata['photoURL']),
-      Text(mpdata['displayName']),
-      Text(mpdata['likescount'].toString()),
-      Text(mpdata['blogscount'].toString()),
-    ],);
   }
 }
